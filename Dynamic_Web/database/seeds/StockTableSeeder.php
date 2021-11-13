b577f79503709dd6ece8328fd304c43b9abeea2a
dangtranhuy1104 <88694323+dangtranhuy1104@users.noreply.github.com>
2021-11-13 09:25:15 +0700

    BKTDW

---
+++ Dynamic_Web/database/seeds/StockTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Stock;
+
+class StockTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Stock::create([
+            'product_name' => 'Note 10',
+            'product_desc' => 'The product of SamSung',
+            'product_qty' => 200,
+            'product_image' => 'picture.jpg'
+        ]);
+    }
+}
